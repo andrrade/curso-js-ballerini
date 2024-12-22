cb28e9e39960f4bdf9316435735b1e298c9191bf
andrrade <devandrrade@gmail.com>
2024-12-22 15:36:52 -0300

    finalizando aula 02

--- aula02/script.js
+++ aula02/script.js
@@ -1,10 +1,29 @@
 function adicionarTarefa() {
-  //   let msg = "Tarefa adicionada com sucesso!";
-  //   document.getElementById("msg").textContent = msg;
-
+  // recebe valor do input do usuário
   let inpTarefa = document.getElementById("inpTarefa");
   let tarefa = inpTarefa.value;
-  //   console.log(tarefa);
 
-  document.getElementById("msg").textContent = tarefa;
+  // cria um novo item (li) e insere na lista não ordenada (ul)
+  let listaTarefas = document.getElementById("listaTarefas");
+  let novaTarefa = document.createElement("li");
+  novaTarefa.textContent = tarefa;
+  listaTarefas.appendChild(novaTarefa);
+
+  // msg de tarefa adicionada
+  let msg = "Tarefa adicionada com sucesso :)";
+  document.getElementById("msg").textContent = msg;
+
+  // limpa o input do usuário e coloca o foco no input
+  inpTarefa.value = "";
+  inpTarefa.focus();
 }
+
+// fazer o botão funcionar ao clicar na tecla Enter
+document
+  .getElementById("inpTarefa")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      // Verifica se a tecla pressionada é "Enter"
+      adicionarTarefa(); // Chama a função para adicionar a tarefa
+    }
+  });
